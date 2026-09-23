0371060f2965831e0df7d3c0111c56d2aeeb1d66
HarshBhushanD <harshbd12@gmail.com>
2026-09-23 19:03:09 +0530

    feat: add CloudCart Terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,24 @@
 output "cloudcart_namespace" {
-  value = kubernetes_namespace.cloudcart.metadata[0].name
+  description = "CloudCart Kubernetes namespace"
+  value       = kubernetes_namespace.cloudcart.metadata[0].name
+}
+
+output "auth_service_name" {
+  description = "CloudCart Auth Kubernetes service"
+  value       = kubernetes_service.auth_service.metadata[0].name
+}
+
+output "product_service_name" {
+  description = "CloudCart Product Kubernetes service"
+  value       = kubernetes_service.product_service.metadata[0].name
+}
+
+output "auth_service_port" {
+  description = "CloudCart Auth service port"
+  value       = kubernetes_service.auth_service.spec[0].port[0].port
+}
+
+output "product_service_port" {
+  description = "CloudCart Product service port"
+  value       = kubernetes_service.product_service.spec[0].port[0].port
 }
